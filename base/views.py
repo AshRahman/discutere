547ceb511edc1f1a394f8ecd3e3bd3b1ae993fd8
AshRahman <ashfaqurrahmanrahat@outlook.com>
2023-08-13 00:26:39 +0600

    added the sql and migration, the home page has some issues

--- base/views.py
+++ base/views.py
@@ -1,16 +1,29 @@
 from django.shortcuts import render
+from .models import Room
 
 rooms = [
-    {'id':1,'name': 'lets learn python!'},
-    {'id':2,'name': 'Django go go!'},
-    {'id':3,'name': 'Water in  the Flask!'},
+    {"id": 1, "name": "lets learn python!"},
+    {"id": 2, "name": "Django go go!"},
+    {"id": 3, "name": "Water in  the Flask!"},
 ]
 
+
 def home(request):
-    context = {'rooms': rooms}
-    return render(request, 'base/home.html', context)
+    rooms = Room.objects.all()
+    context = {"rooms": rooms}
+    return render(request, "base/home.html", context)
+
 
 def room(request, pk):
-    return render(request, 'base/room.html')
+    room = None
+    # rooms = Room.objects.all()
+    for i in rooms:
+        if i["id"] == int(pk):
+            room = i
+    context = {"room": room}  # context used here as dictionary
+    # this for loop transfers the name from the rooms dictionary to html page
+
+    return render(request, "base/room.html", context)
+
 
 # Create your views here.
