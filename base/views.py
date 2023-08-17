389e3b1ca8e7b048447132298fb2d96c9fa98b64
AshRahman <ashfaqurrahmanrahat@outlook.com>
2023-08-17 16:42:23 +0600

    added "Message". "User" & "Topic" models

--- base/views.py
+++ base/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from .models import Room
 
-rooms = [
-    {"id": 1, "name": "lets learn python!"},
-    {"id": 2, "name": "Django go go!"},
-    {"id": 3, "name": "Water in  the Flask!"},
-]
+# rooms = [
+#     {"id": 1, "name": "lets learn python!"},
+#     {"id": 2, "name": "Django go go!"},
+#     {"id": 3, "name": "Water in  the Flask!"},
+# ]
 
 
 def home(request):
@@ -15,13 +15,13 @@ def home(request):
 
 
 def room(request, pk):
-    room = None
+    room = Room.objects.get(id=pk)
     # rooms = Room.objects.all()
-    for i in rooms:
-        if i["id"] == int(pk):
-            room = i
+    # for i in rooms:
+    #     if i["id"] == int(pk):
+    #         room = i
     context = {"room": room}  # context used here as dictionary
-    # this for loop transfers the name from the rooms dictionary to html page
+    # # this for loop transfers the name from the rooms dictionary to html page
 
     return render(request, "base/room.html", context)
 
