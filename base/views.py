60c9837ed163bf7b57b555e9cee7480c51cdcf9c
AshRahman <ashfaqurrahmanrahat@outlook.com>
2023-08-31 23:03:07 +0600

    started the activity feed functionality

--- base/views.py
+++ base/views.py
@@ -67,13 +67,20 @@ def home(request):
 
     topics = Topic.objects.all()
     room_count = rooms.count()
-    context = {"rooms": rooms, "topics": topics, "room_count": room_count}
+    room_messages = Message.objects.all()
+
+    context = {
+        "rooms": rooms,
+        "topics": topics,
+        "room_count": room_count,
+        "room_messages": room_messages,
+    }
     return render(request, "base/home.html", context)
 
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    room_messages = room.message_set.all().order_by("-created")
+    room_messages = room.message_set.all()
     participants = room.participants.all()
     if request.method == "POST":
         message = Message.objects.create(
@@ -129,7 +136,6 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect("home")
     return render(request, "base/delete.html", {"obj": room})
-
 
 
 @login_required(login_url="/login")
